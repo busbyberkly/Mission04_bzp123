493ecfbd041b2ec531135ae9e8ed10ca55565ada
busbyberkly <bzpeterson0901@gmail.com>
2023-02-06 16:27:24 -0700

    adding data validation

--- Mission04_bzp123/Models/GradecalculatorModel.cs
+++ Mission04_bzp123/Models/GradecalculatorModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,11 +8,28 @@ namespace Mission04_bzp123.Models
 {
     public class GradecalculatorModel
     {
+        [Required(ErrorMessage ="a value in Assignments is required to calculate a grade.")]
+        [Range(0,100,ErrorMessage ="Assignments grade value must be between 0 and 100.")]
         public double Assignments { get; set; }
+
+        [Required(ErrorMessage = "a value in Group Projects is required to calculate a grade.")]
+        [Range(0, 100, ErrorMessage = "Group Projects grade value must be between 0 and 100.")]
         public double GroupProjects { get; set; }
+
+        [Required(ErrorMessage = "a value in Quizzes is required to calculate a grade.")]
+        [Range(0, 100, ErrorMessage = "Quizzes grade value must be between 0 and 100.")]
         public double Quizzes { get; set; }
+
+        [Required(ErrorMessage = "a value in Midterm is required to calculate a grade.")]
+        [Range(0, 100, ErrorMessage = "Midterm grade value must be between 0 and 100.")]
         public double MidtermExam { get; set; }
+        
+        [Required(ErrorMessage = "a value in Final is required to calculate a grade.")]
+        [Range(0, 100, ErrorMessage = "Final grade value must be between 0 and 100.")]
         public double FinalExam { get; set; }
+        
+        [Required(ErrorMessage = "a value in INTEX is required to calculate a grade.")]
+        [Range(0, 100, ErrorMessage = "INTEX grade value must be between 0 and 100.")]
         public double INTEX { get; set; }
     }
 }
